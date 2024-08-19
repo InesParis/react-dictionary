c3f9f2b1ebb741933d8cf178a8f5360c4c34857a
InesParis <inesparisarranz@gmail.com>
2024-08-19 17:10:05 +0200

    api integration
    api integration, api documentation check.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+    let apiKey = "4f2360cc5d2fbf9f02a9o90ddad3f50t";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyWordChange(event) {
