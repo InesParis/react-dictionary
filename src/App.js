3e976ce2fe09569113f1d016e330ff26aea0d705
InesParis <inesparisarranz@gmail.com>
2024-08-19 14:49:56 +0200

    Logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="app-logo">
+          <img src={logo} alt="logo" className="img-fluid" />
+        </div>
         <p>dictionary</p>
       </header>
     </div>
