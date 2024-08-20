dcac4d07e374f6565ebda2dad27eb9593af6eba0
InesParis <inesparisarranz@gmail.com>
2024-08-20 13:47:40 +0200

    new component Synonyms
    synonyms UX friendly display

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,19 +1,16 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   console.log(props.meaning.definition);
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+      <p>{props.meaning.definition}</p>
       <p>
-        {props.meaning.definition}
-        {props.meaning.example && (
-          <>
-            <br />
-
-            <em>{props.meaning.example}</em>
-          </>
-        )}
+        <em>{props.meaning.example}</em>
       </p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
